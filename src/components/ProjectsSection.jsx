a17325b54d6010c0a7b56137a02fa67ee21172bf
BlockbusterAndy <124876077+BlockbusterAndy@users.noreply.github.com>
2025-02-26 00:01:22 +0530

    update ProjectsSection with new project details and links; replace existing projects with updated descriptions and live URLs

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -7,28 +7,26 @@ const ProjectsSection = () => {
       title: "Bid Bazaar - An online auction platform",
       description: "A full-stack web application that allows users to create and participate in online auctions. Built with React, ShadCN UI, TailwindCSS for frontend and Node.js, PostgreSQL, Prisma ORM, SocketIO(for real time communication).",
       github: "https://github.com",
-      live: "https://www.google.com",
+      live: "https://bidbazaar.onrender.com",
       onGoing: true
     },
     {
       title: "Portfolio Website",
       description:"Designed and developed a personal portfolio website to showcase my projects and skills. Utilized React, TailwindCSS, and Framer Motion for a responsive and interactive user experience.",
-      github: "https://github.com",
-      live: "https://www.google.com",
-      onGoing: false
-    },
-    {
-      title: "TipJar - Donation Platform for creators",
-      description:"Developed a platform for creators to connect with audiences for financial support. Used Next.js for performance and MongoDB Atlas for data management. Focused on user experience and efficient payment processes.",
-      github: "https://github.com",
-      live: "https://www.google.com",
+      github: "https://github.com/BlockbusterAndy/Portfolio",
       onGoing: false
     },
     {
       title: "Paskeep - Password Manager",
       description:"Developed a secure password manager using React and TailwindCSS. Implemented encryption for data security and a user-friendly interface for easy password management.",
-      github: "https://github.com",
-      live: "https://www.google.com",
+      github: "https://github.com/BlockbusterAndy/PassKeep-Password-Manager-React",
+      live: "https://passkeep.netlify.app/",
+      onGoing: false
+    },
+    {
+      title: "TipJar - Donation Platform for creators",
+      description:"Developed a platform for creators to connect with audiences for financial support. Used Next.js for performance and MongoDB Atlas for data management. Focused on user experience and efficient payment processes.",
+      github: "https://github.com/BlockbusterAndy/tip-jar-nextjs",
       onGoing: false
     }
   ]
